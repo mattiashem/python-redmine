959d75cbfa4c43f96a003c16e39fc25a3fa00fe2
Mattias Hemmingsson <matte.hemmingsson@gmail.com>
2017-05-04 13:24:50 +0200

    Adding env

--- code/run.py
+++ code/run.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-redmine = Redmine('https://redmine.fareoffice.com', key='KEY')
+redmine = Redmine('https://redmine.fareoffice.com', key=os.environ['REDMINE_KEY'])
 project = redmine.project.get('releases')
 print(project.identifier)
 
